6fd7cd261d05e041ac63019c3a84f7ed476f6e6e
Nikita Migunov <creazerodota@gmail.com>
2019-05-05 11:58:57 +0300

    add database models

---
+++ scrum/db_models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Boolean
+
+from scrum.db.base import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), unique=True)
+    hashed_password = Column(String, nullable=False)
+    full_name = Column(String(100))
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
