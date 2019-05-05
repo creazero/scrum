1a125d113dac3dc0dc3b19fa83a2365456b15c12
Nikita Migunov <creazerodota@gmail.com>
2019-05-05 12:46:43 +0300

    add first valid route for users

--- scrum/api/v1/endpoints/users.py
+++ scrum/api/v1/endpoints/users.py
@@ -1,8 +1,23 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, Depends
+
+from scrum.api.utils import get_db
+from scrum.db.session import SessionScope
+from scrum.models.users import User
+from scrum.repositories.users import UserRepository
 
 router = APIRouter()
 
 
-@router.get('/')
-def get_users():
-    return []
+@router.get('/', response_model=List[User])
+def get_users(
+        session: SessionScope = Depends(get_db)
+):
+    """
+    Fetch all users
+    :param session: db session
+    :return: a list of users
+    """
+    repository = UserRepository(session)
+    return repository.fetch_all()
