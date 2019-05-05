086aa1eece3be5c4257d34611237abd5beea3f9f
Nikita Migunov <creazerodota@gmail.com>
2019-05-05 14:21:59 +0300

    remove api prefix so we wouldn't be redirected (302

--- scrum/api/v1/api.py
+++ scrum/api/v1/api.py
@@ -5,4 +5,4 @@ from scrum.api.v1.endpoints import users, auth
 api_router = APIRouter()
 
 api_router.include_router(auth.router, tags=['auth'])
-api_router.include_router(users.router, prefix='/users', tags=['users'])
+api_router.include_router(users.router, tags=['users'])
