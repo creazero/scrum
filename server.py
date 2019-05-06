c2863fb03bef91eb054e2809c99a5a9b7279638c
Nikita Migunov <creazerodota@gmail.com>
2019-05-06 19:58:41 +0300

    fix the uvicorn's running setup
    Now uvicorn is auto-reloading when run programmatically

--- server.py
+++ server.py
@@ -19,4 +19,4 @@ async def session_middleware(request, call_next):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run('server:app', host='127.0.0.1', port=8000, reload=True)
