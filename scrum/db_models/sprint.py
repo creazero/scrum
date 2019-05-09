b899efaff250412bbd88337e9c8058bb27804abf
Nikita Migunov <creazerodota@gmail.com>
2019-05-09 15:05:26 +0300

    add a constructor for sprint's db model

--- scrum/db_models/sprint.py
+++ scrum/db_models/sprint.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from sqlalchemy import Column, Integer, Date, ForeignKey
 
 from scrum.db.base_class import Base
@@ -10,3 +12,7 @@ class Sprint(Base):
     start_date = Column(Date, nullable=False)
     end_date = Column(Date, nullable=False)
     project_id = Column(Integer, ForeignKey('projects.id'), index=True)
+
+    def __init__(self, *, length: int, start_date: dt.date = None, project_id: int = None):
+        self.end_date = start_date + dt.timedelta(weeks=length)
+        self.project_id = project_id
