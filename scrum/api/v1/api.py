b95f58f41c266e2f4e752861aa27de703bac02f8
Nikita Migunov <creazerodota@gmail.com>
2019-05-09 14:27:24 +0300

    init sprint's api endpoint

--- scrum/api/v1/api.py
+++ scrum/api/v1/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from scrum.api.v1.endpoints import users, auth, projects
+from scrum.api.v1.endpoints import users, auth, projects, sprints
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, tags=['auth'])
 api_router.include_router(users.router, tags=['users'])
 api_router.include_router(projects.router, tags=['projects'])
+api_router.include_router(sprints.router, tags=['sprints'])
