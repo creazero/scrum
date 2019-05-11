eb59decbd1153687a65be99b46a5df2acd50022c
Nikita Migunov <creazerodota@gmail.com>
2019-05-11 08:57:50 +0300

    fix tag's association table

--- scrum/db_models/tags_association.py
+++ scrum/db_models/tags_association.py
@@ -4,4 +4,4 @@ from scrum.db.base_class import Base
 
 tags_association = Table('tags_association', Base.metadata,
                          Column('tag_id', Integer, ForeignKey('tags.id'), primary_key=True),
-                         Column('project_id', Integer, ForeignKey('projects.id'), primary_key=True))
+                         Column('task_id', Integer, ForeignKey('tasks.id'), primary_key=True))
