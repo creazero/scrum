7a2e98242209323984f6470cd3638df13e6a92a8
Nikita Migunov <creazerodota@gmail.com>
2019-05-11 09:14:23 +0300

    add tags repository

---
+++ scrum/repositories/tags.py
@@ -0,0 +1,33 @@
+import logging
+
+from fastapi import HTTPException
+from sqlalchemy import exc
+from sqlalchemy.orm import Session
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+
+from scrum.db_models.tag import Tag as DBTag
+from scrum.models.tag import TagCreate
+
+internal_error = HTTPException(
+    status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+    detail='Внутренняя ошибка'
+)
+logger = logging.getLogger(__name__)
+
+
+class TagRepository(object):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create(self, tag_in: TagCreate) -> DBTag:
+        tag = DBTag(name=tag_in.name, color=tag_in.color,
+                    project_id=tag_in.project_id)
+        self.session.begin()
+        self.session.add(tag)
+        try:
+            self.session.commit()
+            return tag
+        except exc.SQLAlchemyError as e:
+            logger.error(e)
+            self.session.rollback()
+            raise internal_error
